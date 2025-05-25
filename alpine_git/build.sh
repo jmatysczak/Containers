1a393be498d5403776dc9824aa91e68f5b577bd4
Jim Matysczak <jmat@alum.mit.edu>
2025-05-25 22:20:15 +0000

    Alpine/git container.

---
+++ alpine_git/build.sh
@@ -0,0 +1,8 @@
+podman build \
+	--build-arg USER_ID=$(id --user) \
+	--build-arg USER_NAME=$(id --user --name) \
+	--build-arg GROUP_ID=$(id --group) \
+	--build-arg GROUP_NAME=$(id --group --name) \
+	--file Dockerfile \
+	--tag alpine_git \
+	.
---
+++ alpine_git/run.sh
@@ -0,0 +1,1 @@
+podman run --userns=keep-id --user $(id --user):$(id --group) --interactive --tty --rm --volume .:/development --workdir /development alpine_git
